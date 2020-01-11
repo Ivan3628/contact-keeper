3e4541456857c0eba6a1eb7b1bdce85af673decf
Ivan3628 <37456657+Ivan3628@users.noreply.github.com>
2020-01-11 18:27:35 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import axios from "axios";
 
 //Get contacts on DOM load
 
-document.addEventListener("DOMContentLoaded", getContacts);
-
 const getContacts = () => {
   axios
     .get("http://localhost:3000/contacts")
     .then(data => ui.showContacts(data))
     .catch(err => console.log(err));
 };
+
+document.addEventListener("DOMContentLoaded", getContacts);
