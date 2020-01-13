d9f1e79315eb7a6f7a67b12d4b3eafaeebe4de2d
Ivan3628 <37456657+Ivan3628@users.noreply.github.com>
2020-01-13 10:44:03 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,39 @@ import axios from "axios";
 //Get contacts on DOM load
 
 const getContacts = () => {
-  axios
-    .get("http://localhost:3000/contacts")
-    .then(data => ui.showContacts(data))
+  axios({
+    method: "get",
+    url: "http://localhost:3000/contacts"
+  })
+    .then(response => ui.showContacts(response.data))
     .catch(err => console.log(err));
 };
 
 document.addEventListener("DOMContentLoaded", getContacts);
+
+//Submit contact
+
+const addContact = () => {
+  const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const phone = document.querySelector("#phone");
+  const type = document.querySelectorAll('input[name="type"]');
+
+  const data = {
+    name: name,
+    email: email,
+    phone: phone,
+    type: type
+  };
+
+  axios
+    .post("http://localhost:3000/contacts", data)
+    .then(response => {
+      ui.clearFields();
+      getContacts();
+    })
+    .catch(err => console.log(err));
+
+  e.preventDefault();
+};
+document.querySelector("#contact-form").addEventListener("submit", addContact);
